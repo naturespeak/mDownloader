2f88d97ae7b2ac3131268defa33e30eb47b0ead0
linuxqc <linuxqc@gmail.com>
2015-07-02 21:54:45 +0800

    Add custom logging feature, phase 1

--- main.cpp
+++ main.cpp
@@ -22,8 +22,6 @@
  */
 
 #include <iostream>
-#include <signal.h>
-
 using namespace std;
 
 #include "mainwindowdownloader.h"
@@ -35,11 +33,31 @@ using namespace std;
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QTranslator>
-
 #include <QLibraryInfo>
+
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        abort();
+    }
+}
 
 int main(int argc, char *argv[])
 {   
+    qInstallMessageHandler(myMessageOutput);
     QApplication a(argc, argv);
 
     QTranslator qtTranslator;
